100a219233a10a79e24ece6af018f0cfe49f5d32
Perkles <jsmbad@gmail.com>
2021-09-01 20:16:11 -0300

    first commit

---
+++ src/main/java/one/digitalinovation/personaipi/PersonaipiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.personaipi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaipiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaipiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinovation/personaipi/PersonaipiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.personaipi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaipiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
